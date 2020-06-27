98191bb7494667921fb147aee48a912133fd6f46
Brian_Izaki <bkizaki@outlook.com>
2020-06-27 01:03:32 -0300

    add decifrar - refatorado cifrar e inputs

--- js/index.js
+++ js/index.js
@@ -1,36 +1,65 @@
 "use strict"
 
-const button = document.querySelector("button");
+const btnEncript = document.querySelector("#encript");
+const btnDescript = document.querySelector("#descript");
 
-button.addEventListener("click", () =>{
-  const texto = document.querySelector("#texto").value;
-  // console.log(cifrar(texto)); 
+btnEncript.addEventListener("click", () =>{
+  handleCripto('C');
+} );
 
-  document.getElementById("txtCifrado").value = cifrar(texto);
+btnDescript.addEventListener("click", () =>{
+  handleCripto('D');
 } );
 
 
-function cifrar(pTexto){
-  let unicodeArray = [];
-  let cifrado = [];
-  for(let i = 0; i <= pTexto.length -1; i++){
-    let fragmento =  String(pTexto).toUpperCase().charCodeAt(i);
-    unicodeArray.push(fragmento);
+function handleCripto(type){
+  const texto = document.querySelector("#texto").value;
+  const valor = document.querySelector("#valor").value;
+  
+  texto && valor 
+    ? document.getElementById("txtCifrado").value = criptografia(texto, valor, type)
+    : alert('Faltou preencher um campo');
+}
+
+
+function criptografia(texto, valor, tipo){
+  const unicode = [];
+  
+  // in pega valores de propriedades
+  // of pega valores de objetos iteraveis
+  tipo != 'C' ? valor *= -1 : valor = parseInt(valor);
+  
+  for (let letra of texto){
+    unicode.push(letra.charCodeAt() + valor);
   };
+  
+  const resultado = unicode.map(letra => {
+    return String.fromCharCode(letra);
+  }).join(''); 
 
-  unicodeArray = unicodeArray.map((unicode) => {
-    if(!((90 == unicode) || (89 == unicode) || (88 == unicode))){ //nor
-      return unicode + 3;
-    }else{
-      return unicode -= 23;
-    }
-  });
+  return resultado;
+}
 
-  for(let i = 0; i <= unicodeArray.length - 1; i++ ){
-    cifrado.push(String.fromCharCode(unicodeArray[i]));
-  }
+// PRIMEIRA FUNÇÃO DE TESTE
+// function criptografia(pTexto){
+//   let unicodeArray = [];
+//   let cifrado = [];
+//   for(let i = 0; i <= pTexto.length -1; i++){
+//     let fragmento =  String(pTexto).toUpperCase().charCodeAt(i);
+//     unicodeArray.push(fragmento);
+//   };
 
-  return cifrado.join('');
-};
+//   unicodeArray = unicodeArray.map((unicode) => {
+//     if(!((90 == unicode) || (89 == unicode) || (88 == unicode))){ //nor
+//       return unicode + 3;
+//     }else{
+//       return unicode -= 23;
+//     }
+//   });
 
+//   for(let i = 0; i <= unicodeArray.length - 1; i++ ){
+//     cifrado.push(String.fromCharCode(unicodeArray[i]));
+//   }
 
+//   return cifrado.join('');
+// };
